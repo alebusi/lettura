4e803ff43d2762048075d8c7a1e6ebda32c85616
alebusi <allebusi@gmail.com>
2018-03-27 15:54:19 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 setTimeout(function(){
         $(".element0").typed({
             strings: ["Credo"," ","che"," ","la"," ","volontà"," ","di"," ","rappresentare"," ","il"," ","reale"," ","e"," ","dotarlo"," ","di"," ","una"," ","forma"," ","leggibile"," ","sia"," ","prima"," ","di"," ","tutto"," ","legata"," ","ad"," ","un"," ","disegno"," ","della"," ","propria"," ","personalità",".",",","Come"," ","diceva"," ","Argan,"," ","chi"," ","rinnega"," ","di"," ","progettare"," ","accetta"," ","di"," ","essere"," ","progettato",".","Con"," ","il"," ","tempo"," ","ho"," ","poi"," ","capito"," ","che"," ","il"," ","privilegio"," ","di"," ","questo"," ","mestiere"," ","sta"," ","nella"," ","possibilità"," ","di"," ","indagare"," ","aspetti"," ","diversi"," ","del"," ","mondo"," ","in"," ","maniera"," ","sempre"," ","nuova",".",",","Da"," ","qui"," ","nasce"," ","il"," ","desiderio"," ","personale"," ","di"," ","rendere"," ","comprensibile"," ","quanto"," ","assimilato"," ","dall’osservazione"," ","del"," ","reale,"," ","adattandola"," ","affinché"," ","tutti"," ","possano"," ","cogliere"," ","gli"," ","aspetti"," ","più"," ","rilevanti"," ","rispetto"," ","al"," ","contesto"," ","in"," ","cui"," ","vengono"," ","comunicati",".",],
-            typeSpeed: 2000, // typing speed
-            backDelay: 1000, // pause before backspacing
+            typeSpeed: 1000, // typing speed
+            backDelay:  500, // pause before backspacing
             loop: true, // loop on or off (true or false)
             loopCount: false, // number of loops, false = infinite
             callback: function(){ } // call function after typing is done
